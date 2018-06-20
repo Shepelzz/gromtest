128ea600f06a4531faa267daa7c110c318a4f6f2
Zhenya Shepel <shepelzz@gmail.com>
2018-06-20 11:22:05 +0300

    17

--- src/lesson17/homework17_2/Solution.java
+++ src/lesson17/homework17_2/Solution.java
@@ -4,45 +4,29 @@ public class Solution {
     public String maxWord(String input){
         if(input == null || input.trim().equals(""))
             return null;
-        for(String w : sortDescending(input.trim().split(" ")))
-            if(checkWord(w))
-                return w;
-        return null;
+
+        String[] words = input.trim().split(" ");
+        String minWord = checkWord(words[0]) ? words[0] : "";
+        for(int i = 1; i < words.length; i++){
+            if(checkWord(words[i]) && words[i].length() > minWord.length()){
+                minWord = words[i];
+            }
+        }
+        return minWord.equals("") ? null : minWord;
     }
 
     public String minWord(String input){
         if(input == null || input.trim().equals(""))
             return null;
-        for(String w : sortAscending(input.trim().split(" ")))
-            if(checkWord(w))
-                return w;
-        return null;
-    }
 
-    private String[] sortAscending(String[] words){
-        if (words == null)
-            return null;
-        for(int i = 0; i < words.length; i++)
-            for (int j = i + 1; j < words.length; j++)
-                if (words[j].length() < words[i].length()) {
-                    String min = words[j];
-                    words[j] = words[i];
-                    words[i] = min;
-                }
-        return words;
-    }
-
-    private String[] sortDescending(String[] words){
-        if (words == null)
-            return null;
-        for(int i = words.length-1; i >= 0; i--)
-            for (int j = i - 1; j >= 0; j--)
-                if (words[j].length() < words[i].length()) {
-                    String max = words[i];
-                    words[i] = words[j];
-                    words[j] = max;
-                }
-        return words;
+        String[] words = input.trim().split(" ");
+        String minWord = checkWord(words[0]) ? words[0] : "";
+        for(int i = 1; i < words.length; i++){
+            if(checkWord(words[i]) && words[i].length() < minWord.length()){
+                minWord = words[i];
+            }
+        }
+        return minWord.equals("") ? null : minWord;
     }
 
     private boolean checkWord(String word){
