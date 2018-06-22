2c9c3648545c444920ccc4c634f8401deee377cf
Zhenya Shepel <shepelzz@gmail.com>
2018-06-22 15:47:11 +0300

    lesson 18 exercise 2

--- src/lesson18/exercise2/Converter.java
+++ src/lesson18/exercise2/Converter.java
@@ -2,20 +2,30 @@ package lesson18.exercise2;
 
 public class Converter {
     public static void main(String[] args) {
-        System.out.println(intToString(111231));
-        System.out.println(stringToInt("11"));
 
-        System.out.println(stringToInt("1050a"));
+        System.out.println("\nintToString test:");
+        System.out.println(intToString(121));
+        System.out.println(intToString(-23));
+        System.out.println(intToString(0));
 
-        System.out.println(intToString(111231));
-        System.out.println(stringToInt("11"));
+        System.out.println("\nstringToInt test:");
+        System.out.println(stringToInt("123"));
+        System.out.println(stringToInt("text"));
+        System.out.println(stringToInt("123text"));
+        System.out.println(stringToInt("-123"));
+        System.out.println(stringToInt(null));
+        System.out.println(stringToInt("2147483648"));
 
-        System.out.println(intToString(111231));
-        System.out.println(stringToInt("11"));
     }
 
     public static String intToString(int input){
-        return String.valueOf(input);
+        try{
+            return String.valueOf(input);
+        } catch (Exception e){
+            System.out.println(input+" can not be converted to int");
+            return null;
+        }
+
     }
 
     public static int stringToInt(String input){
