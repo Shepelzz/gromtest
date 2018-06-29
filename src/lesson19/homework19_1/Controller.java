9b205abd79a8c48d459cab517e037005f413a835
Zhenya_home <shepelzz@gmail.com>
2018-06-29 14:30:51 +0300

    19_hw1

--- src/lesson19/homework19_1/Controller.java
+++ src/lesson19/homework19_1/Controller.java
@@ -13,14 +13,14 @@ public class Controller {
         }
     }
 
-    public void delete(Storage storage, File file){
+    public void delete(Storage storage, File file) throws IllegalArgumentException{
         if(storage == null)
             return;
 
         try {
             storage.deleteFile(file);
         } catch(IllegalArgumentException e){
-            System.err.println(errorMessage(file.getId(), storage.getId(), e.getMessage()));
+            throw new IllegalArgumentException(errorMessage(file.getId(), storage.getId(), e.getMessage()));
         }
     }
 
