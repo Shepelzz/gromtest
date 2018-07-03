57c4a55df40bdf36585bdb88099c942eb9358d6f
Zhenya Shepel <shepelzz@gmail.com>
2018-07-03 09:30:26 +0300

    lesson 19 hw 2

--- src/lesson19/hw2/Controller.java
+++ src/lesson19/hw2/Controller.java
@@ -1,8 +1,40 @@
 package lesson19.hw2;
 
 public class Controller {
-    public File put(Storage storage, File file) throws RuntimeException{
+    public File put(Storage storage, File file){
+        checkPutFile(storage, file);
 
+        storage.addFile(file);
+        return file;
+    }
+
+    public void delete(Storage storage, File file){
+        checkDeleteFile(storage, file);
+
+        storage.deleteFile(file);
+    }
+
+    public void transferAll(Storage storageFrom, Storage storageTo){
+        if(storageFrom == null || storageTo == null)
+            return;
+
+        for(File f : storageFrom.getFiles())
+            if(f != null)
+                transferFile(storageFrom, storageTo, f.getId());
+    }
+
+    public void transferFile(Storage storageFrom, Storage storageTo, long id){
+        if(storageFrom.getFileById(id) == null)
+            throw new RuntimeException("file not found. file id:"+id+" storage id:"+storageFrom.getId());
+
+        File file = storageFrom.getFileById(id);
+
+        put(storageTo, file);
+        delete(storageFrom, file);
+    }
+
+
+    private void checkPutFile(Storage storage, File file){
         if(storage.checkFileIfExists(file))
             throw new RuntimeException("file already exists. file id:"+file.getId()+" storage id:"+storage.getId());
 
@@ -17,38 +49,10 @@ public class Controller {
 
         if(storage.checkFreeStorageCell()==0)
             throw new RuntimeException("storage is full. file id:"+file.getId()+" storage id:"+storage.getId());
-
-        storage.addFile(file);
-        return file;
     }
 
-    public void delete(Storage storage, File file) throws RuntimeException{
+    private void checkDeleteFile(Storage storage, File file){
         if(!storage.checkFileIfExists(file))
             throw new RuntimeException("file not found. file id:"+file.getId()+" storage id:"+storage.getId());
-
-        storage.deleteFile(file);
-    }
-
-    public void transferAll(Storage storageFrom, Storage storageTo) throws RuntimeException{
-        if(storageFrom == null || storageTo == null)
-            return;
-
-        for(File f : storageFrom.getFiles())
-            if(f != null)
-                transferFile(storageFrom, storageTo, f.getId());
-    }
-
-    public void transferFile(Storage storageFrom, Storage storageTo, long id) throws RuntimeException{
-        if(storageFrom.getFileById(id) == null)
-            throw new RuntimeException("file not found. file id:"+id+" storage id:"+storageFrom.getId());
-
-        File file = storageFrom.getFileById(id);
-
-        try{
-            put(storageTo, file);
-            delete(storageFrom, file);
-        } catch (RuntimeException e){
-            System.out.println(e.getMessage());
-        }
     }
 }
