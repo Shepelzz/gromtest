cd16db75b8c3ff58356cec88adcbd81e355cd2e0
Zhenya Shepel <shepelzz@gmail.com>
2018-08-07 13:48:29 +0300

    homework 31

---
+++ src/lesson31/homework31_1/Demo.java
@@ -0,0 +1,17 @@
+package lesson31.homework31_1;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        String text = "test abraham one two. sa7 abraham sa7 sa7 sa8";
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.countSymbols(text));
+        System.out.println(solution.words(text));
+
+    }
+
+}
---
+++ src/lesson31/homework31_1/Solution.java
@@ -0,0 +1,33 @@
+package lesson31.homework31_1;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+
+    public Map<Character, Integer> countSymbols(String text){
+        Map<Character, Integer> result = new TreeMap<>();
+
+        for(Character ch : text.toCharArray())
+            if(Character.isLetter(ch)) {
+                if(result.containsKey(ch))
+                    result.put(ch, result.get(ch)+1);
+                else
+                    result.put(ch, 1);
+            }
+        return result;
+    }
+
+    public Map<String, Integer> words(String text){
+        Map<String, Integer> result = new TreeMap<>();
+
+        for(String word : text.trim().split(" "))
+            if(word.length() > 2)
+                if(result.containsKey(word))
+                    result.put(word, result.get(word)+1);
+                else
+                    result.put(word, 1);
+        return result;
+    }
+
+}
