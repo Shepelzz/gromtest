790442b751de4e2cf0e3e7128a91be55d8f58070
Zhenya_home <shepelzz@gmail.com>
2018-08-11 19:50:36 +0300

    homework 33 1

--- src/lesson33/homework33_1/Demo.java
+++ src/lesson33/homework33_1/Demo.java
@@ -1,7 +1,10 @@
 package lesson33.homework33_1;
 
+import java.io.FileNotFoundException;
+
 public class Demo {
-    public static void main(String[] args) {
-        Solution.writeToFileFromConsole("C:/Users/Zhenya Shepel/Documents/IDEA Projects/Java_Courses/test1.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        //Solution.writeToFileFromConsole("C:/Users/Zhenya Shepel/Documents/IDEA Projects/Java_Courses/test1.txt");
+        Solution.writeToFileFromConsole("C:\\Users\\Asus_1\\IdeaProjects\\files\\1.txt");
     }
 }
--- src/lesson33/homework33_1/Solution.java
+++ src/lesson33/homework33_1/Solution.java
@@ -1,29 +1,57 @@
 package lesson33.homework33_1;
 
-import org.apache.commons.io.IOUtils;
-
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Solution {
 
-    public static void writeToFileFromConsole(String path){
+    public static void writeToFileFromConsole(String path) throws FileNotFoundException{
+
+        validate(path);
         System.out.println("Enter file content to write the file:");
-        writeToFile(path, readFromConsole());
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            String line;
+            while((line = br.readLine()) != null) {
+                if(line.equals("wr"))
+                    return;
+                write(path, line);
+            }
+        } catch(IOException e){
+            System.err.println("Reading from keyboard failed");
+        }
     }
 
-    private static List<String> readFromConsole(){
+    private static void write(String path, String text){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            bw.append(text);
+            bw.append("\r\n");
+        } catch (IOException e){
+            System.err.println("Can`t write to file with path "+path);
+        }
+    }
+
+    private static void validate(String path) throws FileNotFoundException{
+        if(!new File(path).exists())
+            throw new FileNotFoundException("File with path " + path + " not found");
+    }
+
+
+
+
+
+ /*
+    private static StringBuffer readFromConsole(){
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> resultText = new LinkedList();
+        StringBuffer resultText = new StringBuffer();
         try {
             while(true){
                 String line = br.readLine();
                 if(line.equals("wr"))
                     return resultText;
-                resultText.add(line);
+                resultText.append(line);
+                resultText.append("\r\n");
             }
         } catch (IOException e){
             System.err.println("Reading from keyboard failed");
@@ -34,7 +62,7 @@ public class Solution {
         return resultText;
     }
 
-    private static void writeToFile(String path, List<String> text){
+    private static void writeToFile(String path, StringBuffer text){
         File file = new File(path);
         if(!file.exists()) {
             System.err.println("File with path " + path + " not found");
@@ -48,10 +76,9 @@ public class Solution {
             writer = new FileWriter(file, true);
             bufferedWriter = new BufferedWriter(writer);
 
-            for(String line : text){
-                bufferedWriter.append(line);
-                bufferedWriter.append("\r\n");
-            }
+            bufferedWriter.append(text);
+            bufferedWriter.append("\r\n");
+
         } catch (IOException e){
             System.err.println("Can`t write to file with path "+path);
             return;
@@ -60,4 +87,5 @@ public class Solution {
             IOUtils.closeQuietly(writer);
         }
     }
+    */
 }
